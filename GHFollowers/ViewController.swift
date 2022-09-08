f3cce4bb783b0a1eb2ef831225f475e8c500c9ef
Kenneth Scanlon <scanlon771@gmail.com>
2022-09-07 21:38:33 -0500

    Initial commit. Created main screen connected to ViewController

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Kenneth Scanlon on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
