4b951567eccfc7bfd1e273285af3292cafce26ec
Kenneth Scanlon <scanlon771@gmail.com>
2023-05-21 18:32:04 -0500

    Created a custom alert view.

---
+++ GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Kenneth Scanlon on 5/21/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
