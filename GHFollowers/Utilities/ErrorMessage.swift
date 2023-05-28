496efaa0b07b089f865e92b734a5cc708aa25b9d
Kenneth Scanlon <scanlon771@gmail.com>
2023-05-28 17:56:54 -0500

    Added ErrorMessage enum for network errors.

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Kenneth Scanlon on 5/28/23.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUserName = "This user name created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
