4fc6a8de270c1067d6a755fbf809536410bb2a39
Kenneth Scanlon <scanlon771@gmail.com>
2023-05-29 15:41:34 -0500

    Make network call to retrieve avatar images and add them to collection view.

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Kenneth Scanlon on 5/26/23.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
