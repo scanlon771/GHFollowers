91f9e1379973c604ba55bf7034eb342b22c74dcc
Kenneth Scanlon <scanlon771@gmail.com>
2023-05-29 13:10:25 -0500

    Created collectionviewcell and image view for cell.

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Kenneth Scanlon on 5/29/23.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
