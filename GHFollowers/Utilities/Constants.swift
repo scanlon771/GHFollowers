3ebf2f258b11e418f18ed0757a6bcf25b6cebef4
Kenneth Scanlon <scanlon771@gmail.com>
2023-09-02 15:40:16 -0500

    Added GFUserInfoHeaderVC.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Kenneth Scanlon on 7/25/23.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
